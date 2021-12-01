b0ef3583109c7af94204656fd622143f72526c99
Tayse <tayse_rosa@yahoo.com.br>
2021-12-01 14:44:04 -0300

    Conceito Imutabilidade no React JS

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,5 +1,17 @@
 import { useState } from 'react';
 
+//IMUTABILIDADE//
+
+//Digamos - Forma tradicional -  Altera diretamente no array
+//usuarios = ['Tayse Rosa', 'Joana', 'Maria']
+//usuarios.push('Joaquim')
+
+//Conceito de Imutabilidade
+//Criar um novo array e adicionar a nova informação - usar o spread operator
+//Exemplo;
+//novoUsuario = [...usuarios, 'Joaquim']
+
+
 export function Counter(){
   //let it change - deixe usso mudar
   const [ counter, setCounter ] = useState(0);
